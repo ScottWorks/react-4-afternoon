be48b10e347291400c7ce5423b6451e18de14fcd
ScottWorks <scottworks.io@gmail.com>
2018-04-03 15:56:43 -0600

    completed assignment

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`
+      )
+      .then((res) => {
+        this.setState({
+          students: res.data
+        });
+      });
   }
 
   render() {
+    const { students } = this.state;
+    const displayStudents = students.map((student, i) => {
+      return (
+        <div key={i}>
+          <Link to={`/student/${student.id}`}>
+            <h3>
+              {student.first_name} {student.last_name}
+            </h3>
+          </Link>
+        </div>
+      );
+    });
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {displayStudents}
+        <Link to={`/`}>
+          <button>Back</button>
+        </Link>
       </div>
-    )
+    );
   }
 }
